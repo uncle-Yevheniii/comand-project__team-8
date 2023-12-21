199314313ef3f3338130105a382ee7b0f8374851
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2023-12-21 19:30:35 +0100

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,12 +2,18 @@
   const refs = {
     // openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+      modal: document.querySelector("[data-modal]"),
+    
   };
-
+    const backdrop= document.querySelector(".backdrop");
 //   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
-
+    backdrop.addEventListener("click", toggleModal);
+    document.addEventListener("keydown", event => {
+        if (event.code == "Escape") {
+            toggleModal();
+}
+});
   function toggleModal() {
     refs.modal.classList.add("is-hidden");
   }
