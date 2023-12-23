33f25d86e0fc4c1d67b79f6d4bc7cb8bf11b052d
uncle-Jonik <126769681+uncle-Jonik@users.noreply.github.com>
2023-12-23 11:10:07 +0000

    Deploying to gh-pages from @ uncle-Jonik/comand-project__team-8@f2c887fbbe5b15a0f81d52c5c36d5c9bc12870eb 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,10 +1,10 @@
-import"./assets/styles-efbaacc4.js";import{a as p}from"./assets/vendor-a61d8330.js";async function c(){const r="https://food-boutique.b.goit.study/api/products/popular";return(await p.get(r)).data}document.addEventListener("DOMContentLoaded",u);const o=document.querySelector(".wrapperPopularProduct");async function u(){try{const r=await i();if(r===void 0)throw new Error;l(r)}catch{e()}}async function i(){try{return(await c()).reduce((a,t)=>a+d(t),"")}catch{e()}}function d({img:r,name:a,price:t,size:n,popularity:s}){return`
+import"./assets/styles-abcb5a1d.js";import{a as s}from"./assets/vendor-a61d8330.js";async function u(){const e="https://food-boutique.b.goit.study/api/products/popular";return(await s.get(e)).data}document.addEventListener("DOMContentLoaded",l);const c=document.querySelector(".wrapperPopularProduct");async function l(){try{const e=await i();if(e===void 0)throw new Error;m(e)}catch{d()}}async function i(){try{return(await u()).reduce((r,t)=>r+p(t),"")}catch{d()}}function p({img:e,name:r,price:t,size:a,popularity:o}){return`
   <li class="list-item" data-id="">
-      <img class="img-popular" src="${r}" alt="">     
-          <h3 class="popular-title">${a}</h3>
+      <img class="img-popular" src="${e}" alt="">     
+          <h3 class="popular-title">${r}</h3>
           <p class="popular-desc">${t}</p>
-          <p class="popular-desc">${n}</p>
-          <p class="popular-desc">${s}</p>       
+          <p class="popular-desc">${a}</p>
+          <p class="popular-desc">${o}</p>       
   </li>
-  `}function l(r){o.insertAdjacentHTML("beforeend",r)}function e(){console.error("Error:",err),o.innerHTML=""}
+  `}function m(e){c.insertAdjacentHTML("beforeend",e)}function d(){console.error("Error:",err),c.innerHTML=""}(()=>{const e={closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")},r=document.querySelector(".backdrop");e.closeModalBtn.addEventListener("click",o),r.addEventListener("click",t);function t(n){n.target===r&&o()}document.addEventListener("keydown",a);function a(n){n.code==="Escape"&&(o(),document.removeEventListener("keydown",a))}function o(){e.modal.classList.add("is-hidden")}})();
 //# sourceMappingURL=commonHelpers2.js.map
