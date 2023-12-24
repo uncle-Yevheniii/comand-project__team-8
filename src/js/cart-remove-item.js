8cb15d76ca4c2f31cf3bfc9515c94aaa34be32e6
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-12-24 16:22:25 +0100

    Fixes

--- src/js/cart-remove-item.js
+++ src/js/cart-remove-item.js
@@ -4,13 +4,14 @@ import { renderCart } from '../cart.js';
 
 export function cartRemoveItem(data) {
     const cart = document.querySelector('.cart-with-items-list');
-    cart.addEventListener('click', e => {
-        const updatedCart = data.filter(
-            item => item._id !== e.target.dataset.remove
-        );
+    if (cart) {
+        cart.addEventListener('click', e => {
+            const updatedCart = data.filter(
+                item => item._id !== e.target.dataset.remove
+            );
 
-        localStorage.setItem('cart', JSON.stringify(updatedCart));
-        renderCart(updatedCart);
-    });
-
+            localStorage.setItem('cart', JSON.stringify(updatedCart));
+            renderCart(updatedCart);
+        });
+    }
 };
