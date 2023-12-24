c55ba4cbff0072142154495bb3300806b94177e0
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-12-24 16:13:00 +0100

    Add some fixes

--- src/cart.js
+++ src/cart.js
@@ -6,50 +6,50 @@ import { cartRemoveItem } from './js/cart-remove-item';
 
 const cart = document.querySelector('.js-basket');
 
-const data = [
-    {
-      "_id": "640c2dd963a319ea671e383b",
-      "name": "Ackee",
-      "img": "https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e383b.png",
-      "category": "Fresh_Produce",
-      "price": 8.99,
-      "size": "16 oz",
-      "is10PercentOff": false,
-      "popularity": 0
-    },
-    {
-      "_id": "640c2dd963a319ea671e3864",
-      "name": "Black Beans",
-      "img": "https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3864.png",
-      "category": "Pantry_Items",
-      "price": 1.99,
-      "size": "16oz",
-      "is10PercentOff": false,
-      "popularity": 0
-    },
-    {
-      "_id": "640c2dd963a319ea671e37ad",
-      "name": "Black Olives",
-      "img": "https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e37ad.png",
-      "category": "Fresh_Produce",
-      "price": 3.99,
-      "size": "1 jar (16 oz)",
-      "is10PercentOff": false,
-      "popularity": 0
-    },
-    {
-      "_id": "640c2dd963a319ea671e37a",
-      "name": "Black Olives",
-      "img": "https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e37ad.png",
-      "category": "Fresh_Produce",
-      "price": 3.99,
-      "size": "1 jar (16 oz)",
-      "is10PercentOff": false,
-      "popularity": 0
-    }
-]
+// const data = [
+//     {
+//       "_id": "640c2dd963a319ea671e383b",
+//       "name": "Ackee",
+//       "img": "https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e383b.png",
+//       "category": "Fresh_Produce",
+//       "price": 8.99,
+//       "size": "16 oz",
+//       "is10PercentOff": false,
+//       "popularity": 0
+//     },
+//     {
+//       "_id": "640c2dd963a319ea671e3864",
+//       "name": "Black Beans",
+//       "img": "https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3864.png",
+//       "category": "Pantry_Items",
+//       "price": 1.99,
+//       "size": "16oz",
+//       "is10PercentOff": false,
+//       "popularity": 0
+//     },
+//     {
+//       "_id": "640c2dd963a319ea671e37ad",
+//       "name": "Black Olives",
+//       "img": "https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e37ad.png",
+//       "category": "Fresh_Produce",
+//       "price": 3.99,
+//       "size": "1 jar (16 oz)",
+//       "is10PercentOff": false,
+//       "popularity": 0
+//     },
+//     {
+//       "_id": "640c2dd963a319ea671e37a",
+//       "name": "Black Olives",
+//       "img": "https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e37ad.png",
+//       "category": "Fresh_Produce",
+//       "price": 3.99,
+//       "size": "1 jar (16 oz)",
+//       "is10PercentOff": false,
+//       "popularity": 0
+//     }
+// ]
   
-localStorage.setItem('cart',JSON.stringify(data)) 
+// localStorage.setItem('cart',JSON.stringify(data)) 
 
 const USER_CART = JSON.parse(localStorage.getItem('cart'))|| [];
 // const USER_CART = [];
