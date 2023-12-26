28ce510e744e5889aeb7b52079a90449ccd44189
uncle-Jonik <126769681+uncle-Jonik@users.noreply.github.com>
2023-12-26 19:03:05 +0000

    Deploying to gh-pages from @ uncle-Jonik/comand-project__team-8@2a7fcb16962563e4413e17dce98a620c0196674c 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,5 +1,5 @@
-import{c as s}from"./assets/sptite-51f64df6.js";import{a as m,S as x}from"./assets/vendor-3d334923.js";async function P(){const t="https://food-boutique.b.goit.study/api/products/categories";try{return(await m.get(t)).data}catch(e){return console.error("Помилка під час запиту:",e),[]}}async function b({keyword:t,category:e,page:o,limit:r}){const c="https://food-boutique.b.goit.study/api/products";return(await m.get(c,{params:{keyword:t,category:e,page:o,limit:r}})).data}async function I(){const t="https://food-boutique.b.goit.study/api/products/popular";return(await m.get(t)).data}const M=document.querySelector(".filters-form"),i=document.querySelector(".filters-form-input"),n=document.querySelector(".filters-form-select-category"),j=t=>t.map(e=>`<option value="${e.trim()}">${e.replace("_"," ").replace("_"," ")}</option>`).join("");let S=[];const O=async()=>{S=[...await P()];const e=j(S);n.insertAdjacentHTML("beforeend",e),new x({select:"#category",settings:{showSearch:!1}})};O();const g=async t=>{const e=await b(t);console.log(e.results)},J=t=>{t.preventDefault();const e=i.value.trim(),o=n.value,r={keyword:e||null,category:o==="Show all"?null:o.trim()||null};localStorage.setItem("LS_QUERY_KEY",JSON.stringify(r)),g(r)},N=t=>{if(t.target.value===""){const e=n.value,o={keyword:null,category:e==="Show all"?null:e.trim()||null};localStorage.setItem("LS_QUERY_KEY",JSON.stringify(o)),g(o)}},T=t=>{const e=i.value.trim(),o=t.target.value,r={keyword:e||null,category:o.trim()||null};localStorage.setItem("LS_QUERY_KEY",JSON.stringify(r)),g(r)};window.addEventListener("load",()=>{const t=JSON.parse(localStorage.getItem("LS_QUERY_KEY"))||{keyword:null,category:null};i.value=t.keyword,n.value=t.category,g(t),M.addEventListener("submit",J),n.addEventListener("change",T),i.addEventListener("input",N)});const k=document.querySelector(".products-list"),A={keyword:null,category:null,page:1,limit:6};async function H(){return localStorage.getItem("settings")?await w(JSON.parse(localStorage.getItem("settings"))):await w(A)}async function w(t){try{const e=await b(t);k.innerHTML=Y(e.results)}catch(e){console.log(e.message)}}function Y(t){return t.map(({name:e,img:o,category:r,size:c,popularity:a,price:y,is10PercentOff:v,_id:q})=>(r.includes("_")&&(r=r.split("_").join(" ")),`
-    <div class="productlist-card" data-id="${q}">
+import{c as a}from"./assets/sptite-5bd7cadd.js";import{a as m,S as P}from"./assets/vendor-3d334923.js";async function M(){const t="https://food-boutique.b.goit.study/api/products/categories";try{return(await m.get(t)).data}catch(e){return console.error("Помилка під час запиту:",e),[]}}async function _({keyword:t,category:e,page:o,limit:r}){const s="https://food-boutique.b.goit.study/api/products";return(await m.get(s,{params:{keyword:t,category:e,page:o,limit:r}})).data}async function O(){const t="https://food-boutique.b.goit.study/api/products/popular";return(await m.get(t)).data}const J=document.querySelector(".filters-form"),u=document.querySelector(".filters-form-input"),d=document.querySelector(".filters-form-select-category"),N=t=>t.map(e=>`<option value="${e.trim()}">${e.replace("_"," ").replace("_"," ")}</option>`).join("");let S=[];const T=async()=>{S=[...await M()];const e=N(S);d.insertAdjacentHTML("beforeend",e),new P({select:"#category",settings:{showSearch:!1}})};T();const f=async t=>{const e=await _(t);console.log(e.results)},j=t=>{t.preventDefault();const e=u.value.trim(),o=d.value,r={keyword:e||null,category:o==="Show all"?null:o.trim()||null};localStorage.setItem("LS_QUERY_KEY",JSON.stringify(r)),f(r)},A=t=>{if(t.target.value===""){const e=d.value,o={keyword:null,category:e==="Show all"?null:e.trim()||null};localStorage.setItem("LS_QUERY_KEY",JSON.stringify(o)),f(o)}},z=t=>{const e=u.value.trim(),o=t.target.value,r={keyword:e||null,category:o.trim()||null};localStorage.setItem("LS_QUERY_KEY",JSON.stringify(r)),f(r)};window.addEventListener("load",()=>{const t=JSON.parse(localStorage.getItem("LS_QUERY_KEY"))||{keyword:null,category:null};u.value=t.keyword,d.value=t.category,f(t),J.addEventListener("submit",j),d.addEventListener("change",z),u.addEventListener("input",A)});const n=document.querySelector(".products-list"),L=document.querySelector(".products-list-none");let H=6;const Y={keyword:null,category:null,page:1,limit:6};window.addEventListener("resize",R);function W(t){return window.innerWidth<768?t.limit=6:window.innerWidth>=768&&window.innerWidth<1440?t.limit=8:t.limit=9,t}async function b(){return localStorage.getItem("settings")?await k(JSON.parse(localStorage.getItem("settings"))):await k(Y)}async function k(t){try{t=W(t);const e=JSON.parse(localStorage.getItem("settings"));e.limit=t.limit,localStorage.setItem("settings",JSON.stringify(e));const o=await _(t);o.results.length===0?(L.classList.remove("hidden"),n.classList.add("hidden")):(n.classList.contains("hidden")&&(L.classList.add("hidden"),n.classList.remove("hidden")),n.innerHTML=F(o.results))}catch(e){console.log(e.message)}}function R(){const t=JSON.parse(localStorage.getItem("settings"));window.innerWidth<768&&t.limit===6||window.innerWidth>=768&&window.innerWidth<1440&&t.limit===8||window.innerWidth>=1440&&t.limit===9||(console.log(`windowChange ${H}`),b())}function F(t){return t.map(({name:e,img:o,category:r,size:s,popularity:c,price:y,is10PercentOff:v,_id:I})=>(r.includes("_")&&(r=r.split("_").join(" ")),`
+    <div class="productlist-card" data-id="${I}">
     <div class="productlist-card-img-wrapper">
         <img src="${o}" alt="${e}" class="productlist-card-img" width="140">
     </div>
@@ -9,10 +9,10 @@ import{c as s}from"./assets/sptite-51f64df6.js";import{a as m,S as x}from"./asse
           Category: <span class="productlist-card-text-span">${r}</span>
         </p>
         <p class="productlist-card-text">
-          Size: <span class="productlist-card-text-span">${c}</span>
+          Size: <span class="productlist-card-text-span">${s}</span>
         </p>
         <p class="productlist-card-text">
-          Popularity: <span class="productlist-card-text-span">${a}</span>
+          Popularity: <span class="productlist-card-text-span">${c}</span>
         </p>
     </div>
 
@@ -20,15 +20,15 @@ import{c as s}from"./assets/sptite-51f64df6.js";import{a as m,S as x}from"./asse
         <span class="productlist-card-price">$${y}</span>
         <button type="button" class="productlist-card-btn">
             <svg class="productlist-card-icon-cart" width="18" height="18">
-                <use href="${s}#icon-cart"></use>
+                <use href="${a}#icon-cart"></use>
             </svg></button>
     </div>
     ${v?`<svg class="productlist-dicount" width="60" height="60">
-      <use href="${s}#icon-discount"></use>
+      <use href="${a}#icon-discount"></use>
     </svg>`:""}
 </div>
-  `)).join("")}window.addEventListener("load",_);k.addEventListener("click",z);function z(t){const o=t.target.closest(".productlist-card-btn");if(o){const r=o.closest(".productlist-card"),a={id:r.dataset.id,name:r.querySelector(".productlist-card-header").textContent,price:r.querySelector(".productlist-card-price").textContent,category:r.dataset.category,img:r.querySelector(".productlist-card-img").src,size:r.dataset.size};R(a,o)}}function R(t,e){let o=JSON.parse(localStorage.getItem("cart"))||[];if(o.find(c=>c.id===t.id)){const c=e.querySelector(".productlist-card-icon-cart use");c.setAttribute("style","stroke: var(--background); fill: var(--p_color);"),c.setAttribute("href",`${s}#icon-check`)}else o.push(t),localStorage.setItem("cart",JSON.stringify(o)),console.log("Товар доданий в кошик!");_()}function _(){const t=JSON.parse(localStorage.getItem("cart"))||[],e=document.querySelector(".js-header-span");e&&(e.textContent=t.length)}const F={keyword:null,category:null,page:1,limit:6},K=[];function U(){localStorage.setItem("settings",JSON.stringify(F))}function B(){localStorage.setItem("cart",JSON.stringify(K))}const E=document.querySelector(".wrapperPopularProduct");document.querySelector("body");document.addEventListener("DOMContentLoaded",Q);async function Q(){try{const t=await V();if(t===void 0)throw new Error;G(t)}catch{L()}}function D({category:t,img:e,name:o,popularity:r,size:c,_id:a}){return`
-  <li data-id='${a}' class="popular__products-items  ">
+  `)).join("")}window.addEventListener("load",E);n.addEventListener("click",K);function K(t){const o=t.target.closest(".productlist-card-btn");if(o){const r=o.closest(".productlist-card"),c={id:r.dataset.id,name:r.querySelector(".productlist-card-header").textContent,price:r.querySelector(".productlist-card-price").textContent,category:r.dataset.category,img:r.querySelector(".productlist-card-img").src,size:r.dataset.size};U(c,o)}}function U(t,e){let o=JSON.parse(localStorage.getItem("cart"))||[];if(o.find(s=>s.id===t.id)){const s=e.querySelector(".productlist-card-icon-cart use");s.setAttribute("style","stroke: var(--background); fill: var(--p_color);"),s.setAttribute("href",`${a}#icon-check`)}else o.push(t),localStorage.setItem("cart",JSON.stringify(o)),console.log("Товар доданий в кошик!");E()}function E(){const t=JSON.parse(localStorage.getItem("cart"))||[],e=document.querySelector(".js-header-span");e&&(e.textContent=t.length)}const B={keyword:null,category:null,page:1,limit:6},Q=[];function D(){localStorage.setItem("settings",JSON.stringify(B))}function V(){localStorage.setItem("cart",JSON.stringify(Q))}const $=document.querySelector(".wrapperPopularProduct");document.querySelector("body");document.addEventListener("DOMContentLoaded",G);async function G(){try{const t=await Z();if(t===void 0)throw new Error;tt(t)}catch{C()}}function X({category:t,img:e,name:o,popularity:r,size:s,_id:c}){return`
+  <li data-id='${c}' class="popular__products-items  ">
   <div class="img-box">
     <img
       class="popular__products-img"
@@ -44,7 +44,7 @@ import{c as s}from"./assets/sptite-51f64df6.js";import{a as m,S as x}from"./asse
     </p>
     <div class="add-ifo-box">
       <p class="popular__products-text">
-        Size: <span class="js-popular__products-size">${c}</span>
+        Size: <span class="js-popular__products-size">${s}</span>
       </p>
       <p class="popular__products-text">
         Popularity:
@@ -54,11 +54,11 @@ import{c as s}from"./assets/sptite-51f64df6.js";import{a as m,S as x}from"./asse
   </div>
   <button class="popular__products-button">
     <svg class="popular__products-svg" width="12" height="12">
-      <use href="${s}#icon-cart"></use>
+      <use href="${a}#icon-cart"></use>
     </svg>
   </button>
 </li>
-  `}async function V(){try{return(await I()).reduce((e,o)=>e+D(o),"")}catch{L()}}function G(t){E.insertAdjacentHTML("beforeend",t)}function L(){console.error("Error:",err),E.innerHTML=""}const d=document.querySelector(".wrapperPopularProduct");console.log(d);const f=document.querySelector("body");d.addEventListener("click",p);const l=document.querySelector(".products-list");console.log(l);l.addEventListener("click",u);async function u(t){l.removeEventListener("click",u);const e=t.target.closest(".productlist-card");if(e===null||t.target.closest(".productlist-card-btn")){l.addEventListener("click",u);return}console.log(e);const o=e.dataset.id,r=await $(o);console.log(r),f.insertAdjacentHTML("beforeend",h(r)),C()}async function p(t){d.removeEventListener("click",p);const e=t.target.closest("li");if(e===null||t.target.closest(".popular__products-button")){d.addEventListener("click",p);return}const o=e.dataset.id,r=await $(o);console.log(r),f.insertAdjacentHTML("beforeend",h(r)),C()}async function $(t){const e="https://food-boutique.b.goit.study/api/products/";return m.get(`${e}${t}`).then(o=>o.data).catch(o=>{throw new Error(o)})}function h(t){const{name:e,category:o,size:r,popularity:c,desc:a,price:y,img:v}=t;return`
+  `}async function Z(){try{return(await O()).reduce((e,o)=>e+X(o),"")}catch{C()}}function tt(t){$.insertAdjacentHTML("beforeend",t)}function C(){console.error("Error:",err),$.innerHTML=""}const i=document.querySelector(".wrapperPopularProduct");console.log(i);const h=document.querySelector("body");i.addEventListener("click",g);const l=document.querySelector(".products-list");console.log(l);l.addEventListener("click",p);async function p(t){l.removeEventListener("click",p);const e=t.target.closest(".productlist-card");if(e===null||t.target.closest(".productlist-card-btn")){l.addEventListener("click",p);return}console.log(e);const o=e.dataset.id,r=await q(o);console.log(r),h.insertAdjacentHTML("beforeend",w(r)),x()}async function g(t){i.removeEventListener("click",g);const e=t.target.closest("li");if(e===null||t.target.closest(".popular__products-button")){i.addEventListener("click",g);return}const o=e.dataset.id,r=await q(o);console.log(r),h.insertAdjacentHTML("beforeend",w(r)),x()}async function q(t){const e="https://food-boutique.b.goit.study/api/products/";return m.get(`${e}${t}`).then(o=>o.data).catch(o=>{throw new Error(o)})}function w(t){const{name:e,category:o,size:r,popularity:s,desc:c,price:y,img:v}=t;return`
     <div class="backdrop" data-modal>
   <div class="modal-container modal-product" data-modal">
     <svg
@@ -67,7 +67,7 @@ import{c as s}from"./assets/sptite-51f64df6.js";import{a as m,S as x}from"./asse
       height="22"
       data-modal-close
     >
-      <use href="${s}#icon-close"></use>
+      <use href="${a}#icon-close"></use>
     </svg>
     <div class="modal-product-img-text">
       <div class="modal-product-container-img">
@@ -98,12 +98,12 @@ import{c as s}from"./assets/sptite-51f64df6.js";import{a as m,S as x}from"./asse
 
           <p class="modal-product modal-product-desc">
             Popularity:
-            <span class="modal-product-desc modal-product-desc-value">${c}</span>
+            <span class="modal-product-desc modal-product-desc-value">${s}</span>
           </p>
         </div>
 
         <p class="modal-product-description" style="overflow-y:auto; max-height: 200px">
-         ${a}
+         ${c}
 
         </p>
       </div>
@@ -114,12 +114,12 @@ import{c as s}from"./assets/sptite-51f64df6.js";import{a as m,S as x}from"./asse
       <button class="modal-wimdow-add-to-cart-btn">
         Add to
         <svg class="modal-product-addtocart" width="18" height="18">
-          <use href="${s}#icon-cart"></use>
+          <use href="${a}#icon-cart"></use>
         </svg>
       </button>
     </div>
   </div>
 </div>
     
-    `}function C(){const t=document.querySelector("[data-modal-close]"),e=document.querySelector(".backdrop");function o(){const a=document.querySelector("[data-modal]");a&&(a.remove(),t.removeEventListener("click",o),document.removeEventListener("keydown",c),e.removeEventListener("click",r))}function r(a){a.target===e&&(o(),e.removeEventListener("click",r))}function c(a){a.code==="Escape"&&(o(),document.removeEventListener("keydown",c))}d.addEventListener("click",p),l.addEventListener("click",u),t.addEventListener("click",o),e.addEventListener("click",r),document.addEventListener("keydown",c),document.querySelector("[data-modal]")||f.insertAdjacentHTML("beforeend",h(info))}U();B();H();
+    `}function x(){const t=document.querySelector("[data-modal-close]"),e=document.querySelector(".backdrop");function o(){const c=document.querySelector("[data-modal]");c&&(c.remove(),t.removeEventListener("click",o),document.removeEventListener("keydown",s),e.removeEventListener("click",r))}function r(c){c.target===e&&(o(),e.removeEventListener("click",r))}function s(c){c.code==="Escape"&&(o(),document.removeEventListener("keydown",s))}i.addEventListener("click",g),l.addEventListener("click",p),t.addEventListener("click",o),e.addEventListener("click",r),document.addEventListener("keydown",s),document.querySelector("[data-modal]")||h.insertAdjacentHTML("beforeend",w(info))}D();V();b();
 //# sourceMappingURL=commonHelpers2.js.map
