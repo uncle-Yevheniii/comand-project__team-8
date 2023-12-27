60a53ba5f9e2d8639665c0c297e29b815169517e
uncle-Jonik <126769681+uncle-Jonik@users.noreply.github.com>
2023-12-27 19:23:19 +0000

    Deploying to gh-pages from @ uncle-Jonik/comand-project__team-8@7e73d25fec7877a0c02f2fc9e932df45f09cf0aa 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{c as l}from"./assets/sptite-3a8ceaf5.js";const i="/comand-project__team-8/assets/yellow-shopping-basket@1x-fe591d80.png",f="/comand-project__team-8/assets/yellow-shopping-basket@2x-61cba68c.png",g=`
+import{c as d}from"./assets/sptite-3a8ceaf5.js";const i="/comand-project__team-8/assets/yellow-shopping-basket@1x-fe591d80.png",f="/comand-project__team-8/assets/yellow-shopping-basket@2x-61cba68c.png",y=`
     <picture class="empty-basket">
         <source srcset="${i} 1x, ${f} 2x" />
         <img src="${i}" alt="basket" class="basket-empty-img"/>
@@ -10,7 +10,7 @@ import{c as l}from"./assets/sptite-3a8ceaf5.js";const i="/comand-project__team-8
     <p class="empty-basket-text">
         Go to the main page to select your favorite products and add them to the cart.
     </p>
-`;function m(){const t=document.querySelector(".js-cart-info"),e=document.querySelector(".js-header-span"),a=JSON.parse(localStorage.getItem("cart"))||[];t&&(t.textContent=a.length),e&&(e.textContent=a.length)}function y(){const t=document.querySelector(".cart-delete-all-btn");if(t){const e=()=>{localStorage.setItem("cart","[]"),m(),d([]),t.removeEventListener("click",e)};t.addEventListener("click",e)}}function b(t){let e=0;for(const a of t)e+=a.price*a.quantity;return e.toFixed(2)}function $(t){return`
+`;function m(){const t=document.querySelector(".js-cart-info"),e=document.querySelector(".js-header-span"),a=JSON.parse(localStorage.getItem("cart"))||[];t&&(t.textContent=a.length),e&&(e.textContent=a.length)}function g(){const t=document.querySelector(".cart-delete-all-btn");if(t){const e=()=>{localStorage.setItem("cart","[]"),m(),l([]),t.removeEventListener("click",e)};t.addEventListener("click",e)}}function b(t){let e=0;for(const a of t)e+=a.price*a.quantity;return e.toFixed(2)}function $(t){return`
     <div class="basket-full-container">
         <div class="cart-with-items-container">
         
@@ -18,7 +18,7 @@ import{c as l}from"./assets/sptite-3a8ceaf5.js";const i="/comand-project__team-8
                 <p class="cart-delete-all-text">Delete all</p>
                 <button type="button" class="cart-delete-all-btn">
                     <svg class="delete-all-icon-svg">
-                        <use href="${l}#icon-close"></use>
+                        <use href="${d}#icon-close"></use>
                     </svg>
                 </button>
             </div>
@@ -41,19 +41,19 @@ import{c as l}from"./assets/sptite-3a8ceaf5.js";const i="/comand-project__team-8
                     <div class="cart-product-list-remove">
                         <button class="cart-product-remove-btn" type="button" data-remove=${c}>
                         <svg class="cart-product-remove-svg" data-remove=${c}>
-                            <use href="${l}#icon-close" data-remove=${c}></use>
+                            <use href="${d}#icon-close" data-remove=${c}></use>
                         </svg>
                         </button>
                         <div class="cart-product-count-container">
                             <button class="cart-product-count-decrease" type="button" data-decrease=${c}>
                                 <svg class="cart-product-count-decrease-svg" data-decrease=${c} hidden>
-                                    <use href="${l}#icon-" data-decrease=${c}></use>
+                                    <use href="${d}#icon-" data-decrease=${c}></use>
                                 </svg>
                             </button>
                             <p class="cart-product-count-text" data-count="${c}">${v}</p>
                             <button class="cart-product-count-increase" type="button" data-increase=${c}>
                                 <svg class="cart-product-count-increase-svg" data-increase=${c} hidden>
-                                    <use href="${l}#icon-" data-increase=${c}></use>
+                                    <use href="${d}#icon-" data-increase=${c}></use>
                                 </svg>
                             </button>
                         </div>
@@ -83,5 +83,5 @@ import{c as l}from"./assets/sptite-3a8ceaf5.js";const i="/comand-project__team-8
                 </form>
             </div>
     </div>
-    `}function h(t){const e=document.querySelector(".cart-with-items-list");e&&e.addEventListener("click",a=>{const s=a.target.dataset.remove;if(s){const r=t.filter(o=>o._id!==s);localStorage.setItem("cart",JSON.stringify(r)),d(r)}})}function k(t){S(t)}function u(t,e,a,s){const o=a.map(n=>{if(n._id===t){const c=Math.max(0,n.quantity+e);return s.textContent=c,c===0?null:{...n,quantity:c}}return n}).filter(n=>n!==null);localStorage.setItem("cart",JSON.stringify(o)),d(o)}function S(t){t.forEach(({_id:e})=>{const a=document.querySelector(`.cart-product-count-decrease[data-decrease="${e}"]`),s=document.querySelector(`.cart-product-count-increase[data-increase="${e}"]`),r=document.querySelector(`.cart-product-count-text[data-count="${e}"]`);a&&s&&(a.addEventListener("click",()=>u(e,-1,t,r)),s.addEventListener("click",()=>u(e,1,t,r)))})}const p=document.querySelector(".js-basket"),q=JSON.parse(localStorage.getItem("cart"))||[];function d(t){m(),t.length===0?p.innerHTML=g:p.innerHTML=$(t),y(),h(t),k(t)}d(q);console.log("!!!!");(()=>{const t={openModal:document.querySelector(".cart-form-order-input"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")},e=document.querySelector(".backdrop");t.openModal.addEventListener("submit",function(o){o.preventDefault(),t.openModal.reset(),r()}),t.closeModalBtn.addEventListener("click",r),e.addEventListener("click",a);function a(o){o.target===e&&r()}document.addEventListener("keydown",s);function s(o){o.code==="Escape"&&(r(),document.removeEventListener("keydown",s))}function r(){t.modal.classList.toggle("is-hidden"),console.log("!!!!")}})();
+    `}function h(t){const e=document.querySelector(".cart-with-items-list");e&&e.addEventListener("click",a=>{const s=a.target.dataset.remove;if(s){const r=t.filter(o=>o._id!==s);localStorage.setItem("cart",JSON.stringify(r)),l(r)}})}function k(t){S(t)}function u(t,e,a,s){const o=a.map(n=>{if(n._id===t){const c=Math.max(0,n.quantity+e);return s.textContent=c,c===0?null:{...n,quantity:c}}return n}).filter(n=>n!==null);localStorage.setItem("cart",JSON.stringify(o)),l(o)}function S(t){t.forEach(({_id:e})=>{const a=document.querySelector(`.cart-product-count-decrease[data-decrease="${e}"]`),s=document.querySelector(`.cart-product-count-increase[data-increase="${e}"]`),r=document.querySelector(`.cart-product-count-text[data-count="${e}"]`);a&&s&&(a.addEventListener("click",()=>u(e,-1,t,r)),s.addEventListener("click",()=>u(e,1,t,r)))})}const p=document.querySelector(".js-basket"),q=JSON.parse(localStorage.getItem("cart"))||[];function l(t){m(),t.length===0?p.innerHTML=y:p.innerHTML=$(t),g(),h(t),k(t)}l(q);(()=>{const t={openModal:document.querySelector(".cart-form-order-input"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")},e=document.querySelector(".backdrop");t.openModal.addEventListener("submit",function(o){o.preventDefault(),t.openModal.reset(),localStorage.setItem("cart",JSON.stringify([])),l([]),r()}),t.closeModalBtn.addEventListener("click",r),e.addEventListener("click",a);function a(o){o.target===e&&r()}document.addEventListener("keydown",s);function s(o){o.code==="Escape"&&(r(),document.removeEventListener("keydown",s))}function r(){t.modal.classList.toggle("is-hidden")}})();
 //# sourceMappingURL=commonHelpers.js.map
