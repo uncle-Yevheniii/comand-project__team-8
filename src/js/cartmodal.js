5cc8b47827646b1d387b7ec64b9d34a15563a3f8
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2023-12-27 14:21:16 +0100

    Update cartmodal.js

--- src/js/cartmodal.js
+++ src/js/cartmodal.js
@@ -1,16 +1,64 @@
-(() => {
+// (() => {
   
+//   const refs = {
+//     openModal: document.querySelector(".cart-form-order-input"),
+//     //  openModal: document.querySelector(".cart-form-btn"),
+//     closeModalBtn: document.querySelector("[data-modal-close]"),
+//     modal: document.querySelector("[data-modal]"),
+    
+//   };
+//   console.log(refs.openModal);
+//   const backdrop = document.querySelector(".backdrop");
+//   refs.openModal.addEventListener("submit", toggleModal);
+//     // refs.openModal.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
+//   backdrop.addEventListener("click", handleBackdrop);
+
+//   function handleBackdrop(event) {
+//     if (event.target !== backdrop) {
+//       return;
+//     }
+//     toggleModal();
+//   }
+
+//   document.addEventListener("keydown", handleKey);
+
+//   function handleKey(event) {
+//     if (event.code === "Escape") {
+//       toggleModal();
+//       document.removeEventListener("keydown", handleKey)
+//     }
+//   }
+//   function toggleModal() {
+
+//     refs.modal.classList.toggle("is-hidden");
+//         console.log("!!!!");
+//   }
+  
+// })();  
+console.log("!!!!");
+      
+
+
+
+
+(() => {
   const refs = {
-    // openModal: document.querySelector(".cart-form-order-mail-input"),
-     openModal: document.querySelector(".cart-form-btn"),
+    // openModal: document.querySelector(".cart-form-btn"),
+     openModal: document.querySelector(".cart-form-order-input"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
-    
   };
-  console.log(refs.openModal);
+
   const backdrop = document.querySelector(".backdrop");
-  // refs.openModal.addEventListener("submit", toggleModal);
-    refs.openModal.addEventListener("click", toggleModal);
+
+  refs.openModal.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+        refs.openModal.reset();
+    toggleModal();
+
+  });
+
   refs.closeModalBtn.addEventListener("click", toggleModal);
   backdrop.addEventListener("click", handleBackdrop);
 
@@ -26,14 +74,12 @@
   function handleKey(event) {
     if (event.code === "Escape") {
       toggleModal();
-      document.removeEventListener("keydown", handleKey)
+      document.removeEventListener("keydown", handleKey);
     }
   }
-  function toggleModal() {
 
+  function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
-        console.log("!!!!");
+    console.log("!!!!");
   }
-  
-})();  
-      console.log("!!!!");
+})();
