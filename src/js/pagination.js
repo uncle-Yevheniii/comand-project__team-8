cfa096279b110a73bd0294d23286cee3c7ee458f
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2023-12-27 21:14:35 +0200

    fix

---
+++ src/js/pagination.js
@@ -0,0 +1,24 @@
+import Pagination from 'tui-pagination';
+
+const pagination = new Pagination('pagination', {
+  totalItems: 500,
+  itemsPerPage: 10,
+  visiblePages: 5,
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
+});
