7fe66174d9b0a729f3bf5a4f6b791fb759accdb7
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2023-12-28 17:56:15 +0200

    new pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,32 +1,5 @@
-import Pagination from 'tui-pagination';
-const container = document.querySelector('.tui-pagination');
-console.dir(container);
-let page = 1;
-
-const pagination = new Pagination(container, {
-  totalItems: 480,
-  itemsPerPage: 6,
-  visiblePages: 2,
-  template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage:
-      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-    moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</a>',
-    disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</span>',
-    moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
-      '<span class="tui-ico-ellip">...</span>' +
-      '</a>',
-  },
-});
-
-container.addEventListener('click', searchCurrentPage);
-function searchCurrentPage(event) {
-  console.dir(event.curentTarget);
-}
+const ul_pagination = document.querySelector('.pagination-list');
+const first_pagination = document.querySelector('#first');
+const prev_pagination = document.querySelector('#prev');
+const next_pagination = document.querySelector('#next');
+const last_pagination = document.querySelector('#last');
